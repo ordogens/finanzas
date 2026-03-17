802e3832c51de10c3c7daf6a6c70634fcc025c31
ordogen <oarcilagarcia@gmail.com>
2026-03-17 13:24:43 -0500

    img

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import monify from "../assets/monify.png";
 
 type Props = {
   children: ReactNode;
@@ -6,22 +7,25 @@ type Props = {
 
 export const MainLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center">
-      {/* Contenedor principal tipo app */}
-      <div className="w-full max-w-md bg-gray-200 shadow-lg rounded-xl overflow-hidden">
-        {/* 🔝 Header */}
-        <header className="flex items-center justify-between px-4 py-3 border-b bg-white border-b border-gray-300">
-          <div className="border border-black w-10 h-10 rounded-full flex gap-12 items-center ">
-            <img src="" alt="" />
+    <div className="flex min-h-screen justify-center bg-gray-100">
+      <div className="w-full max-w-md overflow-hidden rounded-xl bg-gray-200 shadow-lg">
+        <header className="flex items-center justify-between border-b border-gray-300 bg-white px-4 py-3">
+          <div className="flex items-center gap-3">
+            <div className="flex h-12 w-12 items-center justify-center overflow-hidden rounded-full border border-black bg-white">
+              <img
+                src={monify}
+                alt="Logo de Monify"
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
 
-            <h1 className="text-lg text-gray-800 font-bold">Monify</h1>
+            <h1 className="text-lg font-bold text-gray-800">Monify</h1>
           </div>
 
           <button className="text-gray-500 hover:text-gray-700">⚙️</button>
         </header>
 
-        {/* 📄 Contenido */}
-        <main className="p-4 space-y-4">{children}</main>
+        <main className="space-y-4 p-4">{children}</main>
       </div>
     </div>
   );
