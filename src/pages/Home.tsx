8375e7b6ebf9dd7696a56485b4219fe8f611783c
ordogen <oarcilagarcia@gmail.com>
2026-03-20 12:57:06 -0500

    mejoras visuales

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,15 @@ export const Home = () => {
   const dia = fecha.getDate();
   return (
     <div className="space-y-5">
-      <h2 className="text-xl font-bold ">Hola Ordogen</h2>
-      <p>Resumen del mes: <span className="font-bold capitalize">{mes} {dia}</span></p>
+      <div className="bg-white w-full h-20 rounded-2xl flex flex-col justify-center px-5 shadow-sm">
+        <h2 className="text-xl font-bold">Hola Ordogen</h2>
+        <p>
+          Resumen del mes:{" "}
+          <span className="font-bold capitalize">
+            {mes} {dia}
+          </span>
+        </p>
+      </div>
       <CardBalance />
       <hr className="border border-gray-300" />
       <ListaMovimientos />
